e98104774178b21513ce83724f122d8e4e5de0da
poker2121 <banoobnagy@gmail.com>
2024-11-03 22:11:38 +0200

    problems

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
 
+const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+
+function activateLinkOnScroll() {
+  let scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+  navLinks.forEach(link => {
+    let section = document.querySelector(link.getAttribute('href'));
+
+    if (section.offsetTop <= scrollPosition && section.offsetTop + section.offsetHeight > scrollPosition) {
+      navLinks.forEach(link => link.classList.remove('active')); 
+      link.classList.add('active'); 
+    }
+  });
+}
+
+
+window.addEventListener('scroll', activateLinkOnScroll);
+
 function updateNavbarColor() {
   const navbar = document.getElementById("navbar-example");
   if (window.scrollY < window.innerHeight) {
